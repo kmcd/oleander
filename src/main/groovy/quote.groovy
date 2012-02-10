4e0d90abf1bebe5e9f89a3a0437f4d2e68c06986
Keith McDonnell <keith@dancingtext.com>
2012-02-10 12:12:37 +0000

    Quote creation OK

--- src/main/groovy/quote.groovy
+++ src/main/groovy/quote.groovy
@@ -2,13 +2,25 @@ import redis.clients.jedis.*
 import groovy.json.*
 
 // Saved in Redis as a sorted set:
-// ["SPY", 242343 (UNIX epoch)] -> "time_stamp:'2012-12-31 09:30:00' close:124.44"
+// ["SPY", 242343 (UNIX epoch)] -> "time_stamp:'2012-12-31 09:30:00' close:124.44", "ticker":'SPY'
 
 class Quote {
    static repository = new Jedis("localhost")
    static request_ids = [:]
+   static timestamp_format = "yyyy-MM-dd HH:mm:ss"
    
    static find_all(args) {
+      def from = parse_date(args['from']).time
+      def to = parse_date(args['to']).time
+      def json = new JsonSlurper()
+      
+      args['tickers'].collect { 
+         repository.zrangeByScore(it, from, to).collect { json.parseText(it) }
+      }.flatten().inject([:]) { quotes, quote ->
+         def key = [ quote['time_stamp'], quote['ticker'] ].join(' ')
+         quotes[ key ] = [ bid:quote['bid'], ask:quote['ask'] ]
+         quotes
+      }.sort { it.key }
    }
    
    static create(reqId, time_stamp, open, high, low, close, volume, wap, count) {
@@ -16,12 +28,13 @@ class Quote {
    }
    
    static create(quotes, ticker, time_stamp) {
-      def format = "yyyy-MM-dd HH:mm:ss"
-      def ts = Date.parse(format, time_stamp)
-      def quote = new groovy.json.JsonBuilder()
-      quote time_stamp:ts.format(format), bid:quotes['bid'], ask:quotes['ask']
+      def ts = parse_date(time_stamp)
+      def json = new groovy.json.JsonBuilder()
+      def quote = [ time_stamp:ts.format(timestamp_format), ticker:ticker ]
+      ['bid', 'ask'].each { if(quotes[it]) quote.put(it,quotes[it]) }
       
-      repository.zadd ticker, ts.time, quote.toString()
+      json quote
+      repository.zadd ticker, ts.time, json.toString()
    }
    
    static count(ticker) { repository.zcard ticker }
@@ -35,4 +48,5 @@ class Quote {
    
    static quote_for(id, price)   { [ (request_ids[id][-3..-1]):price ] }
    static ticker_for(id)         { request_ids[id][0..2] }
+   static parse_date(time_stamp) { Date.parse(timestamp_format, time_stamp) }
 }
--- src/test/groovy/quote_test.groovy
+++ src/test/groovy/quote_test.groovy
@@ -4,7 +4,7 @@ class QuoteTest extends GroovyTestCase {
    def price = 123.4567
    def id(type) { type.hashCode() }
    
-   void setUp() {
+   void setUp() {      
       Quote.repository.select 1
       Quote.repository.flushDB()
    }
@@ -22,30 +22,35 @@ class QuoteTest extends GroovyTestCase {
       assert Quote.quote_for(id('ivv bid'), price) == [bid:price]
       assert Quote.quote_for(id('ivv ask'), price) == [ask:price]
    }  
-   
+                                                       
    void test_ticker_for_request_id() {
       assert Quote.ticker_for(id('spy bid')) == 'spy'
       assert Quote.ticker_for(id('spy ask')) == 'spy'
       assert Quote.ticker_for(id('ivv bid')) == 'ivv'
       assert Quote.ticker_for(id('ivv ask')) == 'ivv'
-   }  
-   
+   }                      
+
    void test_unique_time_and_ticker() {
-      Quote.create bid:price, ask:price, 'SPY', "2011-11-14 09:30:00"
-      Quote.create bid:price, ask:price, 'SPY', "2011-11-14 09:30:00"
-      
-      assert Quote.count("SPY") == 1
+      Quote.create bid:price, ask:price, 'spy', "2011-11-14 09:30:00"
+      Quote.create bid:price, ask:price, 'spy', "2011-11-14 09:30:00"
+                    
+      assert Quote.count("spy") == 1
    }  
    
    void test_fetch_quotes_by_date_range_for_ticker_pair() {
-      Quote.create bid:price, ask:price, 'SPY', "2011-11-14 09:30:00"
-      Quote.create bid:price, ask:price, 'IVV', "2011-11-14 09:30:00"
-      Quote.create bid:price, ask:price, 'SPY', "2011-11-14 09:31:00"
-      Quote.create bid:price, ask:price, 'IVV', "2011-11-14 09:31:00"
-      
-      // quotes = Quote.find_all( tickers:[SPY, IVV], from:"2011-11-14 09:30:00", 
-         // to: => "2011-11-14 09:31:00" )
-         // 
-      // assert quotes.size() == 4
+      Quote.create bid:price, ask:price, 'spy', "2011-11-14 09:30:00"
+      Quote.create bid:price, ask:price, 'ivv', "2011-11-14 09:30:00"
+      Quote.create bid:price, ask:price, 'spy', "2011-11-14 09:31:00"
+      Quote.create bid:price, ask:price, 'ivv', "2011-11-14 09:31:00"
+        
+      assertEquals(
+         [ 
+            "2011-11-14 09:30:00 spy": ['bid':price, 'ask':price],
+            "2011-11-14 09:30:00 ivv": ['bid':price, 'ask':price],
+            "2011-11-14 09:31:00 spy": ['bid':price, 'ask':price],
+            "2011-11-14 09:31:00 ivv": ['bid':price, 'ask':price]
+         ],
+         Quote.find_all(from:"2011-11-14 09:30:00", to:"2011-11-14 09:31:00", tickers:['spy', 'ivv'])
+      )
    }
 }
