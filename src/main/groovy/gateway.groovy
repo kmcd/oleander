4e0916cc905a5ab3750cbe3468ca6555d0afc7cd
Keith McDonnell <keith@dancingtext.com>
2012-02-17 15:30:22 +0000

    Goodbye 5 sec bars

--- src/main/groovy/gateway.groovy
+++ src/main/groovy/gateway.groovy
@@ -14,12 +14,6 @@ class Gateway extends IbGateway {
       orders[orderId] = status
    }
    
-   public void realtimeBar(int reqId, long time, double open, double high, double low, double close, long volume, double wap, int count) {
-      def time_stamp = (new Date((time) * 1000)).format('yyyy-MM-dd HH:mm:ss')
-      
-      Quote.create(reqId, time_stamp, open, high, low, close, volume, wap, count)
-   }
-   
    public void historicalData(int reqId, String date, double open, double high, double low, double close, int volume, int count, double WAP, boolean hasGaps) {
       if ( date =~ ~'finished' ) return
       def time_stamp = Date.parse("yyyyMMdd HH:mm:ss", date).format('yyyy-MM-dd HH:mm:ss:S')
@@ -33,12 +27,7 @@ class Gateway extends IbGateway {
    
    public void nextValidId(int orderId) { next_order_id = orderId }
    
-   def real_time_bars(stock, type) {
-      client_socket.reqRealTimeBars(
-         Quote.request_id(stock.symbol(), type), stock.contract, 5, type.toUpperCase(), true )
-   }
-   
-   def streaming_quotes(stock) {
+   def real_time_quotes(stock) {
       client_socket.reqMktData( Quote.request_id(stock.symbol()), stock.contract, '', false )
    }
    
@@ -48,13 +37,6 @@ class Gateway extends IbGateway {
       client_socket.placeOrder(next_order_id, contract, order)
       
       log.info("[ORDER] type:${order.m_action} symbol:${contract.m_symbol} quantity:${order.m_totalQuantity} price:${order.m_lmtPrice}")
-   }
-   
-   def fetch_todays_quotes(stock,type) {
-      def today = new Date().format("yyyyMMdd HH:mm:ss z") as String
-      
-      client_socket.reqHistoricalData( Quote.request_id(stock.symbol(), type),
-         stock.contract, today, "1 D", "30 secs", type.toUpperCase(), 1, 1 )
    }
    
    def wait_for_next_request_id_from_gateway() { sleep(250) }
