3eedffed02089f85a46de01176a63070675bf0b4
Keith McDonnell <keith@dancingtext.com>
2012-02-17 21:16:35 +0000

    DB schema ok

--- src/main/groovy/paper_trade.groovy
+++ src/main/groovy/paper_trade.groovy
@@ -5,24 +5,19 @@ ivv = new Stock("ivv", gateway)
 gateway.connect()
 Quote.delete_all()
 
-gateway.streaming_quotes spy
-gateway.streaming_quotes ivv
+gateway.real_time_quotes spy
+gateway.real_time_quotes ivv
 
 position = new Position(open:false)
 pair_order = new PairOrder(gateway)
 
 // Paper trade
-// while(true) {
-   // if ( position.open ) {
-      // spy_bid = position.spy_profit_target(ivv.ask(), 5.0)
-      // pair_order.exit(spy.bid(), ivv.ask() -0.01)
-   // } 
-   // else {
-      // bids_asks = Quote.current_bids_asks()
-      // spy_ask = spy.ask() - 0.01 - pair.spread(Quote.current_bids_asks())
-      // ivv_bid = ivv.bid() + ( 0.01 + pair.spread(bids_asks) )
-      // 
-      // position = new Position(spy:spy.ask()-0.01, ivv:ivv.bid()+0.01)
-      // pair_order.enter(new Position(spy:spy.ask(), ivv:ivv.bid()))
-   // }
-// }
+while(true) {
+   if ( position.profitable() ) {
+      pair_order.exit( position )
+   } 
+   else {
+      // if entry signal
+      pair_order.enter( position )
+   }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,87 +1,69 @@
-create table spy_bid(
-   time_stamp timestamp,
-   price decimal not null,
-   
-   primary key(time_stamp)
-);
+create table spy_bid(time_stamp timestamp primary key, price decimal not null );
+create table ivv_bid(time_stamp timestamp primary key, price decimal not null );
+create table ivv_ask(time_stamp timestamp primary key, price decimal not null );
+create table long_spreads(time_stamp timestamp primary key, spread decimal not null );
+create table short_spreads(time_stamp timestamp primary key, spread decimal not null );
 
-create table spy_ask(
-   time_stamp timestamp,
-   price decimal not null,
-   
-   primary key(time_stamp)
-);
+create function spy_bid(timestamp)
+returns decimal as $$ 
+select price from spy_bid where time_stamp <= $1 order by time_stamp  desc limit 1; 
+$$ language sql;
 
-create table ivv_bid(
-   time_stamp timestamp,
-   price decimal not null,
-   
-   primary key(time_stamp)
-);
+create function spy_ask(timestamp)
+returns decimal as $$ 
+select price from spy_ask where time_stamp <= $1 order by time_stamp  desc limit 1; 
+$$ language sql;
 
-create table ivv_ask(
-   time_stamp timestamp,
-   price decimal not null,
-   
-   primary key(time_stamp)
-);
+create function ivv_bid(timestamp)
+returns decimal as $$ 
+select price from ivv_bid where time_stamp <= $1 order by time_stamp  desc limit 1; 
+$$ language sql;
 
-create view long_spreads as
-   select spy_ask.time_stamp, spy_ask.price - ivv_bid.price as spread
-   from spy_ask, ivv_bid
-   where spy_ask.time_stamp = ivv_bid.time_stamp;
+create function ivv_ask(timestamp)
+returns decimal as $$ 
+select price from ivv_ask where time_stamp <= $1 order by time_stamp  desc limit 1; 
+$$ language sql;
 
-create view short_spreads as
-   select spy_bid.time_stamp, spy_bid.price - ivv_ask.price as spread
-   from spy_bid, ivv_ask
-   where spy_bid.time_stamp = ivv_ask.time_stamp;
+create rule calculate_long_spread_spy
+as on insert to spy_ask
+do insert into long_spreads values(NEW.time_stamp, NEW.price - ivv_bid(NEW.time_stamp));
+   
+create rule calculate_long_spread_ivv
+as on insert to ivv_bid
+do insert into long_spreads values(NEW.time_stamp, spy_ask(NEW.time_stamp) - NEW.price);
+   
+create rule calculate_short_spread_spy
+as on insert to spy_bid
+do insert into short_spreads values(NEW.time_stamp, NEW.price - ivv_ask(NEW.time_stamp));
+   
+create rule calculate_short_spread_ivv
+as on insert to ivv_ask
+do insert into short_spreads values(NEW.time_stamp, spy_bid(NEW.time_stamp) - NEW.price);
 
-create function current_long_spread() returns decimal as
-   'select spread
-   from long_spreads
-   where time_stamp >= current_date
-   order by time_stamp desc
-   limit 1'
-   language sql;
+create or replace view short_entry_stats as
+   select time_stamp, 
+      spread - avg(spread) over( order by time_stamp ) as score,
+      spread - stddev(spread) over( order by time_stamp ) as stddev
+   from short_spreads;
+  
+create or replace view long_entry_stats as
+select time_stamp, 
+   spread - avg(spread) over( order by time_stamp ) as score,
+   spread - stddev(spread) over( order by time_stamp ) as stddev
+from long_spreads;
+  
+create view long_zscore as
+   select time_stamp, score/stddev as zscore from long_entry_stats;
    
-create function current_short_spread() returns decimal as
-   'select spread
-   from short_spreads
-   where time_stamp >= current_date
-   order by time_stamp desc
-   limit 1'
-   language sql;
-
-create function long_entry_signal() returns numeric as
-   'select avg(spread) - current_long_spread()
-   as entry_signal
-   from long_spreads'
-   language sql;
-   
-create function short_entry_signal() returns numeric as
-   'select avg(spread) - current_short_spread()
-   as entry_signal
-   from short_spreads'
-   language sql;
-   
-create view long_signals as
-   select ls.time_stamp, ls.spread, sa.price as spy_ask, ib.price as ivv_bid, avg(ls.spread)
-   over (order by ls.time_stamp)
-   from long_spreads ls, spy_ask sa, ivv_bid ib
-   where sa.time_stamp = ls.time_stamp 
-   and ib.time_stamp = ls.time_stamp;
+create view short_zscore as
+   select time_stamp, score/stddev as zscore from short_entry_stats;
    
 create view long_entry_signals as
-   select time_stamp, avg - spread as spread, spy_ask, ivv_bid 
-   from long_signals;
-   
-create view short_signals as
-   select ss.time_stamp, ss.spread, sb.price as spy_bid, ia.price as ivv_ask, avg(ss.spread)
-   over (order by ss.time_stamp)
-   from short_spreads ss, spy_bid sb, ivv_ask ia
-   where ss.time_stamp = sb.time_stamp 
-   and ss.time_stamp = ia.time_stamp;
+   select time_stamp, zscore, spy_ask(time_stamp), ivv_bid(time_stamp)
+   from long_zscore
+   order by time_stamp desc; 
    
 create view short_entry_signals as
-   select time_stamp, avg - spread as spread, spy_bid, ivv_ask
-   from short_signals;
+   select time_stamp, zscore, spy_bid(time_stamp), ivv_ask(time_stamp)
+   from short_zscore
+   order by time_stamp desc; 
